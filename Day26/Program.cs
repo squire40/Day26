aa4e65459b6ba1b6e5a9369641148cd0d1eb8c57
Dave Bourke <squire40@hotmail.com>
2018-11-19 20:59:23 -0500

    Day 26 Solved

--- Day26/Program.cs
+++ Day26/Program.cs
@@ -10,6 +10,53 @@ namespace Day26
     {
         static void Main(string[] args)
         {
+            var actual = Console.ReadLine();
+            var expected = Console.ReadLine();
+
+            Console.WriteLine(CalculateFine(actual, expected));
+            Console.ReadKey();
+        }
+
+        private static int CalculateFine(string actual, string expected)
+        {
+            DateTime actualDate = ConvertInputToDate(actual);
+            var expectedDate = ConvertInputToDate(expected);
+
+            if (actualDate <= expectedDate)
+            {
+                return 0;
+            }
+            if (actualDate.Month == expectedDate.Month && actualDate.Year == expectedDate.Year)
+            {
+                return 15 * SubtractDays(actualDate, expectedDate);
+            }
+            if (actualDate.Year == expectedDate.Year)
+            {
+                return 500 * SubtractMonths(actualDate, expectedDate);
+            }
+
+            return 10000;
+        }
+
+        private static int SubtractMonths(DateTime actualDate, DateTime expectedDate)
+        {
+            return actualDate.Month - expectedDate.Month;
+        }
+
+        private static DateTime ConvertInputToDate(string input)
+        {
+            DateTime result;
+
+            var arr = input.Split(' ');
+
+            result = DateTime.Parse($"{arr[1]}/{arr[0]}/{arr[2]}");
+
+            return result;
+        }
+
+        private static int SubtractDays(DateTime actualDate, DateTime expectedDate)
+        {
+            return Convert.ToInt32((actualDate - expectedDate).TotalDays);
         }
     }
 }
